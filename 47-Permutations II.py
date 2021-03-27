e191c988bfe3af55598fb720bfadeefab64f1dca
sarochT <eurobest@euros-Mac-mini.local>
2021-03-27 09:31:41 +0700

    47. Permutations II

---
+++ 47-Permutations II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        answers = []
+        
+        def find_permute(candidates, candidate_indexes):
+            # stop condition
+            if len(candidates) == len(nums):
+                if candidates not in answers:
+                    answers.append(candidates)
+                    return
+                
+            # recursive call
+            for i in range(0, len(nums)):
+                if i not in candidate_indexes:
+                    find_permute(candidates + [nums[i]], candidate_indexes + [i])
+        
+        find_permute([], [])
+        return answers
